e2e61af351fbcbd756a1545ecabbd47dcfd2debc
Isabela Rodrigues <154336126+belarodis@users.noreply.github.com>
2025-07-24 11:36:38 -0300

    Feature/home view (#12)
    * Edits in the main view
    
    * Add background color
    
    * Restructuring the controllers, adding the route in the layout cshtml file, base repository created.
    
    * Base to integration with the layout cshtml file, repository created and comments added
    
    * debugging
    
    * Fixes for imports and enums
    
    * MapControllerRoute changed to match application
    
    * Buttons turn into links, and URL.Action function added
    
    * Directory renamed to product
    
    * Removal of old directory that has been renamed
    
    ---------
    
    Co-authored-by: Fernando Gazzana <fernando.gazzana@gmail.com>

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using bellossomIT.Models.Repository;
+namespace bellossomIT.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepo;
+        public ProductController(IProductRepository productRepo)
+        {
+            _productRepo = productRepo;
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            //var products(p/ abaixo, mudar o nome da variavel p um codigo mais legível) = _productRepo.GetAll(ou get by category)(); fazer dps um get by category, manda o tipo e recebe só a de certo tipo
+            //return View(products);
+            return View();
+        }
+
+        public ActionResult Flower(string category)
+        {
+            //pegar os produtos dessa categoria
+            return View(category);
+        }
+        
+        public ActionResult Fertilizer(string category)
+        {
+            //pegar os produtos dessa categoria
+            return View(category);
+        }
+        
+        public ActionResult Vase(string category)
+        {
+            //pegar os produtos dessa categoria, vamos usar ViewData
+            return View(category);
+        }
+    }
+}   
