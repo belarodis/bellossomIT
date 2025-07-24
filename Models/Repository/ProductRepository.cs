be4d03d145484747f36f1cada4c1aa0e25709d0a
Isabela Rodrigues <154336126+belarodis@users.noreply.github.com>
2025-07-24 14:16:24 -0300

    Feature/services (#13)
    * Adding tasks to the repository
    
    * Adding basic services, business rules pending

--- Models/Repository/ProductRepository.cs
+++ Models/Repository/ProductRepository.cs
@@ -5,6 +5,7 @@ namespace bellossomIT.Models.Repository;
 public class ProductRepository : IProductRepository
 {
     private static readonly List<ProductModel> _products = new List<ProductModel>();
+    private static int _nextId = 31;
 
     static ProductRepository()
     {
@@ -45,9 +46,49 @@ public class ProductRepository : IProductRepository
             });
         }
     }
-
-    public List<ProductModel> GetAll()
+    
+    public Task<List<FlowerModel>> GetAllFlowersAsync()
     {
-        return _products;
+        var flowers = _products.OfType<FlowerModel>().ToList();
+        return Task.FromResult(flowers);
+    }
+
+    public Task<List<FertilizerModel>> GetAllFertilizersAsync()
+    {
+        var fertilizers = _products.OfType<FertilizerModel>().ToList();
+        return Task.FromResult(fertilizers);
+    }
+
+    public Task<List<VaseModel>> GetAllVasesAsync()
+    {
+        var vases = _products.OfType<VaseModel>().ToList();
+        return Task.FromResult(vases);
+    }
+
+    public Task AddAsync(ProductModel product)
+    {
+        product.Id = _nextId++;
+        _products.Add(product);
+        return Task.CompletedTask;
+    }
+    
+    public Task UpdateAsync(ProductModel product)
+    {
+        var index = _products.FindIndex(p => p.Id == product.Id);
+        if (index != -1)
+        {
+            _products[index] = product;
+        }
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        var product = _products.FirstOrDefault(p => p.Id == id);
+        if (product != null)
+        {
+            _products.Remove(product);
+        }
+        return Task.CompletedTask;
     }
 }
